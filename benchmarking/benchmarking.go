0ab5934ba0ed10022f8f297613cdbcec91b461db
JasZal <jasminzalonis@web.de>
2025-09-05 13:27:29 +0200

    commented benchmarking and cleaned-up repo

--- benchmarking/benchmarking.go
+++ benchmarking/benchmarking.go
@@ -40,6 +40,7 @@ import (
 	"math/big"
 	"math/rand"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
@@ -51,112 +52,10 @@ import (
 
 func UNUSED(...interface{}) {}
 
-func write(filename string, message string, append bool) {
-
-	var file *os.File
-	var err error
-
-	if append {
-		file, err = os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
-	} else {
-		file, err = os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(message)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
-
-func generateData(datapoints []int, boundX, boundY int) {
-	//data sets have 10000 attr per line
-
-	prefix := "./datasets/"
-	postfix := ".txt"
-
-	for _, dp := range datapoints {
-		data := make([]string, dp)
-		y := make([]string, dp)
-
-		for i := 0; i < dp; i++ {
-			data[i] = fmt.Sprint(rand.Intn(int(boundX)))
-			y[i] = fmt.Sprint(rand.Intn(boundY))
-		}
-		if dp < 10000 {
-			write(prefix+"data_"+fmt.Sprint(dp)+postfix, strings.Join(data, ", "), false)
-			write(prefix+"func_"+fmt.Sprint(dp)+postfix, strings.Join(y, ", "), false)
-
-		} else {
-			write(prefix+"data_"+fmt.Sprint(dp)+postfix, "", false)
-			write(prefix+"func_"+fmt.Sprint(dp)+postfix, "", false)
-			for i := 0; i < dp/10000; i++ {
-				write(prefix+"data_"+fmt.Sprint(dp)+postfix, strings.Join(data[10000*i:10000*(i+1)], ", ")+"\n", true)
-				write(prefix+"func_"+fmt.Sprint(dp)+postfix, strings.Join(y[10000*i:10000*(i+1)], ", ")+"\n", true)
-			}
-		}
-	}
-	//noise always 0
-}
-
-func loadData(filename string, n, m int) []data.Vector {
-	// Open the file
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	// Read the original matrix
-	var values []int64
-	scanner := bufio.NewScanner(file)
-	const maxBufferSize = 1024 * 1024
-	scanner.Buffer(make([]byte, maxBufferSize), maxBufferSize)
-	nOld := 0
-	mOld := 0
-	for scanner.Scan() {
-		nOld++
-		line := scanner.Text()
-		nums := strings.Fields(line)
-		mOld = len(nums)
-		for _, num := range nums { // Remove any commas or whitespace
-			num = strings.TrimSpace(strings.ReplaceAll(num, ",", ""))
-			val, err := strconv.Atoi(num)
-			if err != nil {
-				log.Fatal(err)
-			}
-			values = append(values, int64(val))
-		}
-
-	}
-
-	// Check if an error occurred during scanning
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error while scanning:", err)
-	}
-
-	// Check if the number of values matches the new matrix dimensions
-	if len(values) != n*m {
-		log.Fatal(fmt.Sprintf("new dimension does not fit to data set (old n=%v, m=%v,  new n=%v, m=%v)", nOld, mOld, n, m))
-	}
-
-	// Reshape into a new matrix
-	matrix := make([]data.Vector, n)
-
-	for i := 0; i < n; i++ {
-		matrix[i] = data.NewConstantVector(m, big.NewInt(0))
-		for j := 0; j < m; j++ {
-			matrix[i][j] = big.NewInt(values[i*m+j])
-		}
-	}
-	return matrix
-}
-
+// performs benchmarking on dyno and stores it in a file
+// if uncommented also measures times for diffpipe
+// rounds: describes the number of rounds that time is averaged about
+// distr: describes how the data set is distritbuted between n and m, possible values: 0, 0.5 and 1
 func main() {
 	results := "resultsReviews.txt"
 	prefix := "./datasets/"
@@ -173,11 +72,11 @@ func main() {
 	boundY := int(math.Pow(2, 7))
 
 	distribution := []float64{0, 0.5} //, 1}
-	rounds := 1
-	nrWorkers := 50
+	rounds := 50
+	nrWorkers := runtime.NumCPU()
 
 	//generateData(datapoints, boundX, boundY)
-	write(results, "benchmarking MCFE_OT, DiffPIPE\nTimes in "+format+"\nDS n,m,Setup, Enc, KeyGen, Dec\n", false)
+	write(results, "benchmarking Dyno, DiffPIPE\nTimes in "+format+"\nDS n,m,Setup, Enc, KeyGen, Dec\n", false)
 	write(results, "NMMCFE\n", true)
 
 	for _, dp := range datapoints {
@@ -194,13 +93,15 @@ func main() {
 			} else if distr == 1 {
 				n = dp
 				m = 1
+			} else {
+				fmt.Println("distribution not supported")
 			}
 
 			//read data and func
 			plaintexts := loadData(prefix+"data_"+fmt.Sprint(dp)+postfix, n, m)
 			yplain := loadData(prefix+"func_"+fmt.Sprint(dp)+postfix, n, m)
 
-			//test MCFE
+			//test dyno
 			tS, tE, tKG, tD := 0.0, 0.0, 0.0, 0.0
 			for r := 0; r < rounds; r++ {
 				timeS, timeE, timeKG, timeD := NMCFE(plaintexts, yplain, boundX, boundY, nrWorkers)
@@ -211,12 +112,14 @@ func main() {
 				tD += float64(timeD.Nanoseconds()) / float64(rounds)
 			}
 
-			fmt.Printf("NMCFE_OT: setup: %.3f %v, enc %.3f %v, keygen %.3f %v, dec %.3f %v\n", tS/conversion[format], format, tE/conversion[format], format, tKG/conversion[format], format, tD/conversion[format], format)
+			fmt.Printf("Dyno: setup: %.3f %v, enc %.3f %v, keygen %.3f %v, dec %.3f %v\n", tS/conversion[format], format, tE/conversion[format], format, tKG/conversion[format], format, tD/conversion[format], format)
 
 			//write results
 			write(results, fmt.Sprintf(" %v & %v& %.3f&%.3f&%.3f&%.3f\\\\\n", n, m, tS/conversion[format], tE/conversion[format], tKG/conversion[format], tD/conversion[format]), true)
 
 			//test DiffPIPE
+			//please uncomment if this times should be included
+
 			// tS, tE, tKG, tD = 0.0, 0.0, 0.0, 0.0
 			// for r := 0; r < rounds; r++ {
 			// 	timeS, timeE, timeKG, timeD := DiffPIPE(plaintexts, yplain, boundX, boundY, nrWorkers)
@@ -238,6 +141,7 @@ func main() {
 	UNUSED(datapoints, results)
 }
 
+// computes runtimes for Diffpipe on given data set with given function
 func DiffPIPE(plaintexts, yplain []data.Vector, boundX, boundY, nrWorkers int) (time.Duration, time.Duration, time.Duration, time.Duration) {
 	var tSetup, tEnc, tKeyGen, tDec time.Duration
 	secLevel := 2
@@ -294,6 +198,7 @@ func DiffPIPE(plaintexts, yplain []data.Vector, boundX, boundY, nrWorkers int) (
 	return tSetup, tEnc, tKeyGen, tDec
 }
 
+// computes runtimes for Dyno on given data set with given function
 func NMCFE(plaintexts, yplain []data.Vector, boundX, boundY, nrWorkers int) (time.Duration, time.Duration, time.Duration, time.Duration) {
 	var tSetup, tEnc, tKeyGen, tDec time.Duration
 
@@ -381,4 +286,112 @@ func workersDiffPIPE(x []data.Vector, cipher data.MatrixG1, msk *noisy.OTNHMulti
 			log.Fatalf("Error during encryption: %v", err)
 		}
 	}
+}
+
+// writes results into file
+func write(filename string, message string, append bool) {
+
+	var file *os.File
+	var err error
+
+	if append {
+		file, err = os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+	} else {
+		file, err = os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// generates data for benchmarking
+func generateData(datapoints []int, boundX, boundY int) {
+	//data sets have 10000 attr per line
+
+	prefix := "./datasets/"
+	postfix := ".txt"
+
+	for _, dp := range datapoints {
+		data := make([]string, dp)
+		y := make([]string, dp)
+
+		for i := 0; i < dp; i++ {
+			data[i] = fmt.Sprint(rand.Intn(int(boundX)))
+			y[i] = fmt.Sprint(rand.Intn(boundY))
+		}
+		if dp < 10000 {
+			write(prefix+"data_"+fmt.Sprint(dp)+postfix, strings.Join(data, ", "), false)
+			write(prefix+"func_"+fmt.Sprint(dp)+postfix, strings.Join(y, ", "), false)
+
+		} else {
+			write(prefix+"data_"+fmt.Sprint(dp)+postfix, "", false)
+			write(prefix+"func_"+fmt.Sprint(dp)+postfix, "", false)
+			for i := 0; i < dp/10000; i++ {
+				write(prefix+"data_"+fmt.Sprint(dp)+postfix, strings.Join(data[10000*i:10000*(i+1)], ", ")+"\n", true)
+				write(prefix+"func_"+fmt.Sprint(dp)+postfix, strings.Join(y[10000*i:10000*(i+1)], ", ")+"\n", true)
+			}
+		}
+	}
+}
+
+// loads data for benchmarking
+func loadData(filename string, n, m int) []data.Vector {
+	// Open the file
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Read the original matrix
+	var values []int64
+	scanner := bufio.NewScanner(file)
+	const maxBufferSize = 1024 * 1024
+	scanner.Buffer(make([]byte, maxBufferSize), maxBufferSize)
+	nOld := 0
+	mOld := 0
+	for scanner.Scan() {
+		nOld++
+		line := scanner.Text()
+		nums := strings.Fields(line)
+		mOld = len(nums)
+		for _, num := range nums { // Remove any commas or whitespace
+			num = strings.TrimSpace(strings.ReplaceAll(num, ",", ""))
+			val, err := strconv.Atoi(num)
+			if err != nil {
+				log.Fatal(err)
+			}
+			values = append(values, int64(val))
+		}
+
+	}
+
+	// Check if an error occurred during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error while scanning:", err)
+	}
+
+	// Check if the number of values matches the new matrix dimensions
+	if len(values) != n*m {
+		log.Fatal(fmt.Sprintf("new dimension does not fit to data set (old n=%v, m=%v,  new n=%v, m=%v)", nOld, mOld, n, m))
+	}
+
+	// Reshape into a new matrix
+	matrix := make([]data.Vector, n)
+
+	for i := 0; i < n; i++ {
+		matrix[i] = data.NewConstantVector(m, big.NewInt(0))
+		for j := 0; j < m; j++ {
+			matrix[i][j] = big.NewInt(values[i*m+j])
+		}
+	}
+	return matrix
 }
